684cfd579856fe7527164081b1525302fe991678
AmarinLee <amarinl.25@ichat.sp.edu.sg>
2026-05-07 13:36:52 +0800

    Edited for Lab3

--- bmi.py
+++ bmi.py
@@ -5,11 +5,15 @@ def calculate_bmi(height, weight):
     print(f"BMI = {BMI}")
     if BMI < 18.5:
         print("Underweight")
+        return -1
     elif BMI <= 24.9:
         print("Normal")
+        return 0
     elif BMI <= 29.9:
         print("Overweight")
+        return 1
     else:
         print("Obese")
+        return 2
 
-calculate_bmi(weight=70, height=1.78)
+result = calculate_bmi(weight=70, height=1.78)
